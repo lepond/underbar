e52d9b4f162434a437dd2769ce9c045d8fd79ec0
Lindsay Pond <lepond@gmail.com>
2015-02-18 09:59:01 -0800

    Completed _.map function using _.each and pushing each value to a new array.

--- src/underbar.js
+++ src/underbar.js
@@ -101,6 +101,9 @@
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
+    var mappedArray = [];
+    _.each(collection, function(index){ mappedArray.push(iterator(index)); });
+    return mappedArray;
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
