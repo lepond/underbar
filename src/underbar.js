d6536a800beb3495f52763bbda51a72f5c278038
Lindsay Pond <lepond@gmail.com>
2015-02-18 07:51:27 -0800

    Completed _.reject using _.filter and defining filter test as test(index) != true = false.

--- src/underbar.js
+++ src/underbar.js
@@ -84,6 +84,7 @@
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
+    return _.filter(collection, function (index) {return test(index) != true;});
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
   };
